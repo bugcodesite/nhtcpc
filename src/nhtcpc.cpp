c24b36cae974cbc937a6c6d90f930fbddab98b4c
bugecode <373335273@qq.com>
2022-12-07 02:52:05 +0000

    ok

--- src/nhtcpc.cpp
+++ src/nhtcpc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <arpa/inet.h>
@@ -17,12 +18,74 @@ typedef struct clientinfo
 {
     unsigned char state=0;
     int fd;
+	int mfd;
     long t;
+	char peerip[32];
+	int  peerport;
     struct clientinfo* n=NULL;
 } CLIENTINFO,*PCLIENTINFO;
-
+PCLIENTINFO clients=NULL;
+PCLIENTINFO connects=NULL;
+struct epoll_event ev;
+void on_service_event(int epfd,int cfd,char *buf,int rl){
+	PCLIENTINFO preclient=NULL;
+	PCLIENTINFO client=clients;
+	int flag=0;
+	while(client!=NULL){
+		if(cfd==client->fd){
+			flag=1;
+			break;
+		}else{
+			preclient=client;
+			client=client->n;
+		}
+	}
+	if(flag==1){
+		if(rl<=0){
+			if(preclient==NULL){
+				clients=client->n;
+			}else{
+				preclient->n=client->n;
+			}
+			//client closed
+			cout<<"service "<<client->fd<<" "<<client->t<<" "<<client->peerip<<":"<<client->peerport<<" closed"<<endl;
+			epoll_ctl(epfd,EPOLL_CTL_DEL,cfd,&ev);
+			free(client);
+		}else{
+			buf[rl]=0;
+			cout<<buf<<endl;
+		}
+		return;
+	}
+	preclient=NULL;
+	client=connects;
+	while(client!=NULL){
+		if(cfd==client->fd){
+			flag=2;
+			break;
+		}else{
+			preclient=client;
+			client=client->n;
+		}
+	}
+	if(flag==2){
+		if(rl<=0){
+			if(preclient==NULL){
+				connects=client->n;
+			}else{
+				preclient->n=client->n;
+			}
+			//client closed
+			cout<<"connect "<<client->fd<<" "<<client->t<<" "<<client->peerip<<":"<<client->peerport<<" closed"<<endl;
+			epoll_ctl(epfd,EPOLL_CTL_DEL,cfd,&ev);
+			free(client);
+		}else{
+		
+		}
+	}
+}
 int main(int argc,char**argv){
-    PCLIENTINFO clients=PCLIENTINFO(calloc(sizeof(CLIENTINFO),POLL_SIZE));
+   
 	int port=7901;
 	unsigned char buf[MAXDATASIZE];
 	char tbuf[256];
@@ -36,9 +99,7 @@ int main(int argc,char**argv){
 	}
 	cout<<"listen 0.0.0.0:"<<port<<endl;
 	int sockfd = socket( AF_INET, SOCK_STREAM, 0 );
-	if(sockfd==-1){
-		
-	}else{
+	if(sockfd!=-1){
 		gethostname(tbuf,sizeof(tbuf));
 		cout<<tbuf<<endl;
 		struct hostent *he = gethostbyname("0.0.0.0");
@@ -59,7 +120,7 @@ int main(int argc,char**argv){
 		cout<<"start working"<<endl;
 		int epfd = epoll_create(POLL_SIZE);
 		int nfds;
-		struct epoll_event ev;
+		
 		ev.data.fd=sockfd;
 		ev.events=EPOLLIN|EPOLLET;
 		epoll_ctl(epfd,EPOLL_CTL_ADD,sockfd,&ev);
@@ -73,20 +134,22 @@ int main(int argc,char**argv){
 				if (cfd== sockfd) {
 					aaddrlen=sizeof(aaddr);
 					int nfd=accept(sockfd,(struct sockaddr *)&aaddr,&aaddrlen);
-					cout<<"accept"<<" "<<nfd<<" "<<inet_ntoa(aaddr.sin_addr)<<":"<<ntohs(aaddr.sin_port)<<endl;
+					//cout<<"accept"<<" "<<nfd<<" "<<inet_ntoa(aaddr.sin_addr)<<":"<<ntohs(aaddr.sin_port)<<endl;
                     if(nfd>0){
                         fcntl(nfd,F_SETFL,fcntl(nfd,F_GETFL)|O_NONBLOCK);
                         ev.data.fd=nfd;
                         epoll_ctl(epfd,EPOLL_CTL_ADD,nfd,&ev);
+						PCLIENTINFO client=PCLIENTINFO(calloc(1,POLL_SIZE));
+						client->fd=nfd;
+						client->state=1;
+						client->t=time(NULL);
+						client->n=clients;
+						sprintf(client->peerip,"%s",inet_ntoa(aaddr.sin_addr));
+						client->peerport=ntohs(aaddr.sin_port);
+						clients=client;
                     }
                 }else if(events[n].events&EPOLLIN==EPOLLIN){
-                    int rl=recv(cfd,buf,MAXDATASIZE,0);
-                    if(rl<=0){
-                        close(cfd);
-                    }else{
-                        buf[rl]=0;
-                        cout<<cfd<<" "<<buf<<endl;
-                    }
+                    on_service_event(epfd,cfd,(char *)buf,recv(cfd,buf,MAXDATASIZE,0));
                 }else{
 					cout<<events[n].data.fd <<endl;
 				}
